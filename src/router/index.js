3b83422b958ae37dcbfca01f7c707ec1688eda22
Zouhair <ettarak.zouhair@gmail.com>
2022-04-10 15:17:12 +0000

    Create component Events and Event Card

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventsView from "../views/EventsView.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: "/events",
+    name: "EventsView",
+    component: EventsView,
   },
   {
     path: "/about",
